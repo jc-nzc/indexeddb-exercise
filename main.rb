c610dbde7cc3265e7897626fc48f0a48803d908d
JC <node.zerocool@gmail.com>
2021-01-12 23:55:57 -0600

    loops

--- main.rb
+++ main.rb
@@ -1,2 +1,5 @@
-list = [1,2,3]
-list.select(&:even?)
+list = [1,2,3,4,5,6,7,8,9,10]
+puts list.select(&:even?)
+puts "__"
+numbers = [1,2,3,4,5,6,7,8,9,10]
+numbers.each{ |n| puts "Current number is: #{n}" }
