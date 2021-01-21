2f53d345d98b9186f36ff82f547fc11b9bd6582a
JC <node.zerocool@gmail.com>
2021-01-21 15:09:36 -0600

    sol 2

--- python_solutions.py
+++ python_solutions.py
@@ -1,3 +1,18 @@
 # Multiples of 3 or 5
 def solution(number):
     return sum(x for x in range(number) if x % 3 ==0 or x % 5 == 0)
+
+# with a full class
+class Multiples:
+    def __init__(self, maximum):
+      self.maximum = maximum
+
+    def sum(self, base):
+        count = self.maxiumum // base + 1
+        last = base * (count - 1)
+        return count * last // 2
+
+def solution(number):
+    multiples = Multiples(number - 1)
+    return multiples.sum(3) + multiples.sum(5) - multiples.sum(15)
+
