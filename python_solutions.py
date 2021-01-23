3b9b38ac0bd8078635bf53d9d540d977f255ef70
JC <node.zerocool@gmail.com>
2021-01-23 00:09:06 -0600

    conditions

--- python_solutions.py
+++ python_solutions.py
@@ -16,3 +16,17 @@ def solution(number):
     multiples = Multiples(number - 1)
     return multiples.sum(3) + multiples.sum(5) - multiples.sum(15)
 
+# python program to illustrate nested If statement 
+#!/usr/bin/python 
+i = 10
+if (i == 10): 
+    #  First if statement 
+    if (i < 15): 
+        print ("i is smaller than 15") 
+    # Nested - if statement 
+    # Will only be executed if statement above 
+    # it is true 
+    if (i < 12): 
+        print ("i is smaller than 12 too") 
+    else: 
+        print ("i is greater than 15") 
